0b0342bc41bcb32f5792b32d5f3adec2e6cb2437
ZongoForSpeed <tanguy.lambert@gmail.com>
2015-09-16 22:06:00 +0200

    Added few

---
+++ Probleme088.cpp
@@ -0,0 +1,115 @@
+#include "Problemes.h"
+#include "Arithmetiques.h"
+#include "Timer.h"
+#include "Utilitaires.h"
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <limits>
+
+typedef unsigned long long nombre;
+typedef std::vector<nombre> vecteur;
+
+namespace
+{
+    bool verifier(nombre produit, nombre somme, nombre k)
+    {
+        if (somme < k) 
+            return false;
+        if (produit == 1) 
+            return somme == k;
+        if (k == 1) 
+            return produit == somme;
+        
+        for (nombre d = 2; d <= produit && d + k <= somme + 1; ++d) 
+        {
+            if(produit%d == 0 && verifier(produit / d, somme - d, k - 1))
+                return true;
+        }
+        return false;
+    }
+}
+
+void probleme088()
+{
+    Timer t("probleme 88");
+    // A natural number, N, that can be written as the sum and product of a given set of at least 
+    // two natural numbers, {a1, a2, ... , ak} is called a product-sum number: 
+    // N = a1 + a2 + ... + ak = a1 × a2 × ... × ak.
+    //
+    // For example, 6 = 1 + 2 + 3 = 1 × 2 × 3.
+    // 
+    // For a given set of size, k, we shall call the smallest N with this property a minimal 
+    // product-sum number. The minimal product-sum numbers for sets of size, k = 2, 3, 4, 5, and 6
+    // are as follows.
+    // 
+    //      k=2: 4 = 2 × 2 = 2 + 2
+    //      k=3: 6 = 1 × 2 × 3 = 1 + 2 + 3
+    //      k=4: 8 = 1 × 1 × 2 × 4 = 1 + 1 + 2 + 4
+    //      k=5: 8 = 1 × 1 × 2 × 2 × 2 = 1 + 1 + 2 + 2 + 2
+    //      k=6: 12 = 1 × 1 × 1 × 1 × 2 × 6 = 1 + 1 + 1 + 1 + 2 + 6
+    // 
+    // Hence for 2≤k≤6, the sum of all the minimal product-sum numbers is 4+6+8+12 = 30; note that 
+    // 8 is only counted once in the sum.
+    // 
+    // In fact, as the complete set of minimal product-sum numbers for 2≤k≤12 is {4, 6, 8, 12, 15, 16},
+    // the sum is 61.
+    //
+    // What is the sum of all the minimal product-sum numbers for 2≤k≤12000?
+    std::set<nombre> resultat;
+    for (nombre k = 2; k <= 12000; ++k)
+    {
+        for (nombre n = k + 1;; n++)
+        {
+            if (verifier(n, n, k)) 
+            {
+                resultat.insert(n);
+                break;
+            }
+        }    
+    }
+    
+    std::cout << "Solution: " << std::accumulate(resultat.begin(), resultat.end(), 0) << std::endl;
+    /*
+    for (nombre k = 2; k <= 10; ++k)
+    {
+        vecteur solution(k, 1);
+        nombre d = 2;
+        nombre position = 0;
+        nombre produit = 1;
+        nombre somme = k;
+        while (true)
+        {
+            somme = somme + 1;
+            produit = produit * d / solution[position]; 
+            solution[position] = d;
+            // std::cout << solution << " = " << somme << "," << produit << std::endl;
+            if (produit == somme)
+                break;
+            if (produit < somme)
+            {
+                ++position;
+                d = 2;
+            }
+            else
+            {
+                ++d;
+                if (position > 0)
+                {
+                    while (position > 0 && solution[position - 1] < d)
+                    {
+                        somme = somme - solution[position] + 1;
+                        produit = produit / solution[position]; 
+                        solution[position] = 1;
+                        --position;
+                    }
+                }
+                
+            }
+        }
+        std::cout << "k = " << k << "\t" << solution << " = " << produit << std::endl;
+        resultat.insert(produit);
+    }
+    */
+}
---
+++ Probleme090.cpp
@@ -0,0 +1,85 @@
+#include "Problemes.h"
+#include "Arithmetiques.h"
+#include "Timer.h"
+#include "Utilitaires.h"
+
+#include <iostream>
+#include <algorithm>
+#include <limits>
+
+typedef unsigned long long nombre;
+typedef std::vector<nombre> vecteur;
+
+void probleme090()
+{
+    Timer t("probleme 90");
+    // Each of the six faces on a cube has a different digit (0 to 9) written on it; the same is 
+    // done to a second cube. By placing the two cubes side-by-side in different positions we can
+    // form a variety of 2-digit numbers.
+    //
+    // For example, the square number 64 could be formed:
+    //
+    // In fact, by carefully choosing the digits on both cubes it is possible to display all of the
+    // square numbers below one-hundred: 01, 04, 09, 16, 25, 36, 49, 64, and 81.
+    // 
+    // For example, one way this can be achieved is by placing {0, 5, 6, 7, 8, 9} on one cube and 
+    // {1, 2, 3, 4, 8, 9} on the other cube.
+    // 
+    // However, for this problem we shall allow the 6 or 9 to be turned upside-down so that an 
+    // arrangement like {0, 5, 6, 7, 8, 9} and {1, 2, 3, 4, 6, 7} allows for all nine square numbers 
+    // to be displayed; otherwise it would be impossible to obtain 09.
+    // 
+    // In determining a distinct arrangement we are interested in the digits on each cube, not the order.
+    // 
+    //      {1, 2, 3, 4, 5, 6} is equivalent to {3, 6, 4, 1, 2, 5}
+    //      {1, 2, 3, 4, 5, 6} is distinct from {1, 2, 3, 4, 5, 9}
+    //
+    // But because we are allowing 6 and 9 to be reversed, the two distinct sets in the last example 
+    // both represent the extended set {1, 2, 3, 4, 5, 6, 9} for the purpose of forming 2-digit numbers.
+    // 
+    // How many distinct arrangements of the two cubes allow for all of the square numbers to be displayed?
+    std::set<vecteur> des;
+    vecteur de = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    do
+    {
+        vecteur v(de.begin(), de.begin() + 6);
+        std::sort(v.begin(), v.end());
+        des.insert(v);
+    }
+    while(std::next_permutation(de.begin(), de.end()));
+    
+    auto test = [] (const vecteur & de1, const vecteur & de2, nombre c1, nombre c2)
+    {
+        return ((std::find(de1.begin(), de1.end(), c1) != de1.end() && std::find(de2.begin(), de2.end(), c2) != de2.end())
+            || (std::find(de1.begin(), de1.end(), c2) != de1.end() && std::find(de2.begin(), de2.end(), c1) != de2.end()));
+    };
+    
+    nombre resultat = 0;
+    for (auto it1 = des.begin(), en = des.end(); it1 != en; ++it1)
+    for (auto it2 = it1; it2 != en; ++it2)
+    {
+        // 01, 04, 09, 16, 25, 36, 49, 64, and 81
+        if (!test(*it1, *it2, 0, 1)) 
+            continue;
+        if (!test(*it1, *it2, 0, 4)) 
+            continue;
+        if (!test(*it1, *it2, 0, 9) && !test(*it1, *it2, 0, 6)) 
+            continue;
+        if (!test(*it1, *it2, 1, 6) && !test(*it1, *it2, 1, 9)) 
+            continue;
+        if (!test(*it1, *it2, 2, 5)) 
+            continue;
+        if (!test(*it1, *it2, 3, 6) && !test(*it1, *it2, 3, 9)) 
+            continue;
+        if (!test(*it1, *it2, 4, 9) && !test(*it1, *it2, 4, 6)) 
+            continue;
+        if (!test(*it1, *it2, 6, 4) && !test(*it1, *it2, 9, 4)) 
+            continue;
+        if (!test(*it1, *it2, 8, 1)) 
+            continue;
+
+        ++resultat;
+    }
+    
+    std::cout << "Solution: " << resultat << std::endl;
+}
