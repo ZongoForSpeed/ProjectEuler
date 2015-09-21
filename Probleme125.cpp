32649dc498c5391fdd98b57c111786d0ff926a67
ZongoForSpeed <tanguy.lambert@gmail.com>
2015-09-22 00:24:55 +0200

    Added 119, 120, 121, 122, 123, 124 & 125

---
+++ Probleme125.cpp
@@ -0,0 +1,46 @@
+#include "Problemes.h"
+#include "Arithmetiques.h"
+#include "Timer.h"
+#include "Utilitaires.h"
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <limits>
+
+typedef unsigned long long nombre;
+typedef std::vector<nombre> vecteur;
+
+void probleme125()
+{
+    Timer t("probleme 125");
+    // The palindromic number 595 is interesting because it can be written as the sum of 
+    // consecutive squares: 6² + 7² + 8² + 9² + 10² + 11² + 12².
+    //
+    // There are exactly eleven palindromes below one-thousand that can be written as 
+    // consecutive square sums, and the sum of these palindromes is 4164. Note that 1 = 0² + 1²
+    // has not been included as this problem is concerned with the squares of positive integers.
+    // 
+    // Find the sum of all the numbers less than 10^8 that are both palindromic and can be written 
+    // as the sum of consecutive squares.
+    nombre limite = puissance::puissance(10ULL, 8); 
+    vecteur carres;
+    for (nombre n = 1; n*n < limite; ++n)
+        carres.push_back(n*n);
+        
+    std::set<nombre> resultat;
+    for (auto it1 = carres.begin(), en = carres.end(); it1 != en; ++it1)
+    {
+        nombre somme = *it1;
+        for (auto it2 = std::next(it1); it2 != en; ++it2)
+        {
+            somme += *it2;
+            if (somme >= limite)
+                break;
+            else if (arithmetiques::palindrome(somme))
+                resultat.insert(somme);
+        }
+    }
+    
+    std::cout << "Solution: " << std::accumulate(resultat.begin(), resultat.end(), 0ULL) << std::endl;
+}
