452009cd5d4ce16423c0d528a0cb59dacb0095e7
ZongoForSpeed <tanguy.lambert@gmail.com>
2016-05-27 22:56:18 +0200

    Added 291

---
+++ probleme291.cpp
@@ -0,0 +1,39 @@
+#include "problemes.h"
+#include "arithmetiques.h"
+#include "premiers.h"
+#include "puissance.h"
+#include "utilitaires.h"
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <cmath>
+
+typedef unsigned long long nombre;
+
+namespace
+{
+    bool premier(nombre n)
+    {
+        boost::multiprecision::mpz_int z = n;
+        return mpz_probab_prime_p(z.backend().data(), 10) != 0;
+    }
+}
+
+ENREGISTRER_PROBLEME(291, "Panaitopol Primes")
+{
+    // A prime number p is called a Panaitopol prime if (x**4 - y**4)/(x**3 + y**3)
+    // for some positive integers x and y.
+    //
+    // Find how many Panaitopol primes are less than 5×10**15.
+    const nombre limite = 5*puissance::puissance<nombre>(10, 15);
+    
+    nombre resultat = 0;
+    for (nombre n = 1; n*n + (n+1)*(n+1) < limite; ++n)
+    {
+        if (premier(n*n + (n+1)*(n+1)))
+            ++resultat;
+    }
+    
+    std::cout << "Solution: " << resultat << std::endl;
+}
