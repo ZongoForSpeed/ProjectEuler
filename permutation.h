f9bd42191c53cd7a99a136fab2816e06d44102f9
Zongo <zongo@cragganmore>
2017-10-13 23:56:03 +0200

    Added 491 + permutation algo

--- permutation.h
+++ permutation.h
@@ -1,10 +1,28 @@
 #include <boost/range/join.hpp>
 #include <boost/iterator/iterator_facade.hpp>
 
-#include <algorithm>    // std::next_permutation, std::sort
+#include <algorithm>
 
 namespace permutation 
 {
+    template<typename T>
+    bool arrangement_suivant(const std::vector<T> & source, std::vector<bool> & indices, std::vector<T> & subSource) {
+        while (std::prev_permutation(indices.begin(), indices.end())) {
+            std::vector<T> resultat;
+            std::for_each2(source.begin(), source.end(), indices.begin(), 
+                   [&resultat] (T value, bool test) {
+                       if (test) {
+                           resultat.push_back(value);
+                       }
+                   });
+            if (!std::equal(resultat.begin(), resultat.end(), subSource.begin())) {
+                std::swap(resultat, subSource);
+                return true;
+            }
+        }
+        return false;
+    }
+    
     template <typename T>
     class Permutation
     {
@@ -53,6 +71,141 @@ namespace permutation
     
         Iterateur end() {
             return Iterateur(source, std::string::npos);
+        }
+    };
+    
+    template <typename T>
+    class Combinaisons
+    {
+        std::vector<T>  source;
+        size_t          taille;
+        
+    public:
+        Combinaisons(const std::vector<T> &v, size_t t): source(v), taille(t) {}
+        ~Combinaisons() {}
+        
+        class Iterateur: public boost::iterator_facade<Iterateur, const std::vector<T>, boost::forward_traversal_tag>
+        {
+            const std::vector<T> &  _source;
+            size_t                  _taille;
+            size_t                  _index;
+            
+            std::vector<bool>       _indices;
+            std::vector<T>          _courant;
+        public:
+            Iterateur(const std::vector<T> & s, size_t t, size_t index) : _source(s), _taille(t), _index(index) {
+                size_t n = utilitaires::distance(_source.begin(), _source.end());
+                _indices.assign(n, false);
+                for (size_t i = 0; i < t; ++i) {
+                    _indices[i] = true;
+                }
+                
+                _courant.assign(_source.begin(), std::next(_source.begin(), _taille));
+            }
+        private:
+            friend class boost::iterator_core_access;
+            
+            void increment() {
+                if (_index == std::string::npos) {
+                    return;
+                }
+                
+                
+                if (arrangement_suivant(_source, _indices, _courant)) {
+                    ++_index;
+                } else {
+                    _index = std::string::npos;
+                }
+            }
+    
+            bool equal(Iterateur const& other) const {
+                return this->_index == other._index;
+            }
+    
+            const std::vector<T>& dereference() const {
+                return _courant;
+            }
+        };
+        
+        Iterateur begin() {
+            if (taille > source.size()) {
+                return end();
+            } else {
+                return Iterateur(source, taille, 0);
+            }
+        }
+    
+        Iterateur end() {
+            return Iterateur(source, taille, std::string::npos);
+        }
+    };
+    
+    template <typename T>
+    class Arrangements
+    {
+        std::vector<T>  source;
+        size_t          taille;
+        
+    public:
+        Arrangements(const std::vector<T> &v, size_t t): source(v), taille(t) {}
+        ~Arrangements() {}
+        
+        class Iterateur: public boost::iterator_facade<Iterateur, const std::vector<T>, boost::forward_traversal_tag>
+        {
+            const std::vector<T> &  _source;
+            size_t                  _taille;
+            size_t                  _index;
+            
+            std::vector<bool>       _indices;
+            std::vector<T>          _courant;
+        public:
+            Iterateur(const std::vector<T> & s, size_t t, size_t index) : _source(s), _taille(t), _index(index) {
+                size_t n = utilitaires::distance(_source.begin(), _source.end());
+                _indices.assign(n, false);
+                for (size_t i = 0; i < t; ++i) {
+                    _indices[i] = true;
+                }
+                
+                _courant.assign(_source.begin(), std::next(_source.begin(), _taille));
+            }
+        private:
+            friend class boost::iterator_core_access;
+            
+            void increment() {
+                if (_index == std::string::npos) {
+                    return;
+                }
+                
+                if (std::next_permutation(_courant.begin(), _courant.end())) {
+                    ++_index;
+                } else {
+                    if (arrangement_suivant(_source, _indices, _courant)) {
+                        ++_index;
+                    } else {
+                        _index = std::string::npos;
+                    }
+                }
+            }
+    
+            bool equal(Iterateur const& other) const {
+                return this->_index == other._index;
+            }
+    
+            const std::vector<T>& dereference() const {
+                return _courant;
+            }
+        };
+        
+        Iterateur begin() {
+            if (taille > source.size()) {
+                return end();
+            } else {
+                return Iterateur(source, taille, 0);
+            }
+        }
+    
+        Iterateur end() {
+            return Iterateur(source, taille, std::string::npos);
         }
     };
 }
--- test/permutation.cpp
+++ test/permutation.cpp
@@ -7,8 +7,7 @@
 #include <vector>
 
 BOOST_AUTO_TEST_SUITE(test_permutation)
-    BOOST_AUTO_TEST_CASE(permutation_simple)
-    {
+    BOOST_AUTO_TEST_CASE(permutation_simple) {
         const std::vector<std::vector<int>> resultat {
             {1, 2, 3},
             {1, 3, 2},
@@ -27,8 +26,7 @@ BOOST_AUTO_TEST_SUITE(test_permutation)
         }
     }
     
-    BOOST_AUTO_TEST_CASE(permutation_doublon)
-    {
+    BOOST_AUTO_TEST_CASE(permutation_doublon) {
         const std::vector<std::vector<int>> resultat {
             {1, 2, 2, 4},
             {1, 2, 4, 2},
@@ -47,6 +45,85 @@ BOOST_AUTO_TEST_SUITE(test_permutation)
         size_t index = 0;
         const std::vector<int> v {1, 2, 2, 4};
         for (auto i: permutation::Permutation<std::vector<int>>(v)) {
+            BOOST_CHECK_EQUAL_COLLECTIONS(i.begin(), i.end(), 
+                                          resultat[index].begin(), resultat[index].end());
+            ++index;
+        }
+    }
+    
+    BOOST_AUTO_TEST_CASE(arrangements_simple) {
+        const std::vector<std::vector<int>> resultat {
+            {1, 2, 3},
+            {1, 3, 2},
+            {2, 1, 3},
+            {2, 3, 1},
+            {3, 1, 2},
+            {3, 2, 1},
+            {1, 2, 4},
+            {1, 4, 2},
+            {2, 1, 4},
+            {2, 4, 1},
+            {4, 1, 2},
+            {4, 2, 1},
+            {1, 3, 4},
+            {1, 4, 3},
+            {3, 1, 4},
+            {3, 4, 1},
+            {4, 1, 3},
+            {4, 3, 1},
+            {2, 3, 4},
+            {2, 4, 3},
+            {3, 2, 4},
+            {3, 4, 2},
+            {4, 2, 3},
+            {4, 3, 2}
+        };
+        
+        size_t index = 0;
+                
+        std::vector<int> iterable {1, 2, 3, 4};
+        for (auto & i: permutation::Arrangements<int>(iterable, 3)) {
+           BOOST_CHECK_EQUAL_COLLECTIONS(i.begin(), i.end(), 
+                                          resultat[index].begin(), resultat[index].end());
+            ++index;        }
+    }
+    
+    BOOST_AUTO_TEST_CASE(arrangements_doublon) {
+        const std::vector<std::string> resultat {"AB", "BA", "AD", "DA", "BB", "BD", "DB"};
+        
+        size_t index = 0;
+        
+        std::vector<char> iterable {'A', 'B', 'B', 'D'};
+        for (auto & i: permutation::Arrangements<char>(iterable, 2)) {
+            BOOST_CHECK_EQUAL_COLLECTIONS(i.begin(), i.end(), 
+                                          resultat[index].begin(), resultat[index].end());
+            ++index;
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(combinaisons_simple) {
+        const std::vector<std::vector<int>> resultat {
+            {1, 2, 3},
+            {1, 2, 4},
+            {1, 3, 4},
+            {2, 3, 4}
+        };
+        
+        size_t index = 0;
+        std::vector<int> iterable {1, 2, 3, 4};
+        for (auto i : permutation::Combinaisons<int>(iterable, 3)) {
+            BOOST_CHECK_EQUAL_COLLECTIONS(i.begin(), i.end(), 
+                                          resultat[index].begin(), resultat[index].end());
+            ++index;
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(combinaisons_doublon) {
+        const std::vector<std::string> resultat {"AB", "AC", "AD", "BC", "BD", "CD"};
+        
+        size_t index = 0;
+        std::vector<char> iterable {'A', 'B', 'C', 'D'};
+        for (auto i : permutation::Combinaisons<char>(iterable, 2)) {
             BOOST_CHECK_EQUAL_COLLECTIONS(i.begin(), i.end(), 
                                           resultat[index].begin(), resultat[index].end());
             ++index;
