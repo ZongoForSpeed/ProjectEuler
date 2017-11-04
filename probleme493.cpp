31ff2d78d97843786f59a85ebad47e7ef4e4ca4e
ZongoForSpeed <13649539+ZongoForSpeed@users.noreply.github.com>
2017-11-04 14:42:35 +0100

    Added 493

---
+++ probleme493.cpp
@@ -0,0 +1,14 @@
+#include "problemes.h"
+#include "utilitaires.h"
+#include "mp_nombre.h"
+
+ENREGISTRER_PROBLEME(493, "Under The Rainbow") {
+    // 70 colored balls are placed in an urn, 10 for each of the seven rainbow colors.
+    //
+    // What is the expected number of distinct colors in 20 randomly picked balls?
+    //
+    // Give your answer with nine digits after the decimal point (a.bcdefghij).
+    long double resultat = 7 * (1.0l - mp_nombre::coefficient_binomial(60u, 20u).get_double() 
+        / mp_nombre::coefficient_binomial(70u, 20u).get_double());
+    return std::to_string(resultat, 9);
+}
