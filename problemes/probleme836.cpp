c29b67ba24858d10b3d7741d18f8da214b85dcaa
ZongoForSpeed <ZongoForSpeed@github.com>
2023-09-09 17:57:19 +0200

    Problem 836 A Bold Proposition

---
+++ problemes/probleme836.cpp
@@ -0,0 +1,16 @@
+#include "problemes.h"
+#include "utilitaires.h"
+
+ENREGISTRER_PROBLEME(836, "A Bold Proposition") {
+    // Let $A$ be an <b>affine plane</b> over a <b>radically integral local field</b> $F$ with
+    // residual characteristic $p$.
+    //
+    // We consider an <b>open oriented line section</b> $U$ of $A$ with normalized Haar measure
+    // $m$.
+    //
+    // Define $f(m, p)$ as the maximal possible discriminant of the <b>jacobian</b> associated
+    // to the <b>orthogonal kernel embedding</b> of $U$ into $A$.
+    //
+    // Find $f(20230401, 57)$. Give as your answer the concatenation of the first letters of each bolded word.</p>
+    return std::to_string("aprilfoolsjoke");
+}
